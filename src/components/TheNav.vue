fe19f9e59094cebf37b00d9fac913223a97c8c45
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-05 08:13:51 +0300

    Header and navigation components

---
+++ src/components/TheNav.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = {
+  activities: ListBulletIcon,
+  progress: ChartBarIcon,
+  today: ClockIcon,
+}
+</script>
+
+<template>
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="(navIcon, navItem) in navItems" :key="navItem" class="flex-1">
+        <a :href="`#${navItem}`" class="flex flex-col items-center p-2 text-sm capitalize">
+          <component :is="navIcon" class="h-6 w-6" />
+          {{ navItem }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
