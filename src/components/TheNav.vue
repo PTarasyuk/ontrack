9b55625dfbc8d8f59ab75390f2231757a3c77518
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-05 08:26:13 +0300

    Attribute inheritance

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
+import NavItem from './NavItem.vue'
+
 const navItems = {
   activities: ListBulletIcon,
   progress: ChartBarIcon,
@@ -11,12 +13,10 @@ const navItems = {
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="(navIcon, navItem) in navItems" :key="navItem" class="flex-1">
-        <a :href="`#${navItem}`" class="flex flex-col items-center p-2 text-sm capitalize">
-          <component :is="navIcon" class="h-6 w-6" />
-          {{ navItem }}
-        </a>
-      </li>
+      <NavItem v-for="(navIcon, navItem) in navItems" :key="navItem" :href="`#${navItem}`">
+        <component :is="navIcon" class="h-6 w-6" />
+        {{ navItem }}
+      </NavItem>
     </ul>
   </nav>
 </template>
