450823d96784e26a5b98bc6492f2a05fdb8caccd
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-05 12:40:22 +0300

    Constants and functions files

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,24 +1,17 @@
 <script setup>
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants.js'
 import NavItem from './NavItem.vue'
+import { NAV_ITEMS } from '@/constants'
 
 defineProps(['currentPage'])
 
 const emit = defineEmits(['navigate'])
-
-const navItems = {
-  [PAGE_TIMELINE]: ClockIcon,
-  [PAGE_ACTIVITIES]: ListBulletIcon,
-  [PAGE_PROGRESS]: ChartBarIcon,
-}
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
       <NavItem
-        v-for="(navIcon, navItem) in navItems"
+        v-for="(navIcon, navItem) in NAV_ITEMS"
         :key="navItem"
         :href="`#${navItem}`"
         :class="{ 'bg-gray-200 pointer-events-none': navItem === currentPage }"
