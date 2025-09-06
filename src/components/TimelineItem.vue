5a1378c8f1372307860073fa68a28dc6cd199e7c
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-06 10:08:48 +0300

    Handle select box changes

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import BaseSelect from './BaseSelect.vue'
 import { isTimelineItemValid } from '@/validators.js'
 
@@ -23,12 +24,17 @@ const options = [
   { value: 3, label: 'Training' },
 ]
 
-const selectedOptionId = 2
+const selectedOptionId = ref(1)
 </script>
 
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
     <a href="#" :class="hourLinkClasses"> {{ timelineItem.hour }}:00 </a>
-    <BaseSelect :selected="selectedOptionId" :options="options" :placeholder="'Rest'" />
+    <BaseSelect
+      :selected="selectedOptionId"
+      :options="options"
+      :placeholder="'Rest'"
+      @select="selectedOptionId = $event"
+    />
   </li>
 </template>
