8199f535c735ac84e61482da950a677067055c84
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-09 12:05:30 +0300

    Disable stopwatch buttons

--- src/components/TimelineStopwatch.vue
+++ src/components/TimelineStopwatch.vue
@@ -6,7 +6,7 @@ import {
   MILLISECONDS_IN_SECOND,
 } from '@/constants'
 import { formatSeconds } from '@/functions'
-import { isNumber } from '@/validators'
+import { isHourValid, isNumber } from '@/validators'
 import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/vue/24/outline'
 import BaseButton from '@/components/BaseButton.vue'
 import { ref } from 'vue'
@@ -17,10 +17,17 @@ const props = defineProps({
     type: Number,
     validator: isNumber,
   },
+  hour: {
+    required: true,
+    type: Number,
+    validator: isHourValid,
+  },
 })
 
 const seconds = ref(props.seconds)
 const isRunning = ref(false)
+
+const isStartButtonDisabled = props.hour !== new Date().getHours()
 
 function start() {
   isRunning.value = setInterval(() => {
@@ -41,7 +48,7 @@ function reset() {
 
 <template>
   <div class="flex w-full gap-2">
-    <BaseButton :type="BUTTON_TYPE_DANGER" @click="reset">
+    <BaseButton :type="BUTTON_TYPE_DANGER" :disabled="!seconds" @click="reset">
       <ArrowPathIcon class="h-8" />
     </BaseButton>
     <div class="flex flex-grow items-center rounded bg-gray-100 px-2 font-mono text-3xl">
@@ -50,7 +57,7 @@ function reset() {
     <BaseButton v-if="isRunning" :type="BUTTON_TYPE_WARNING" @click="stop">
       <PauseIcon class="h-8" />
     </BaseButton>
-    <BaseButton v-else :type="BUTTON_TYPE_SUCCESS" @click="start">
+    <BaseButton v-else :type="BUTTON_TYPE_SUCCESS" :disabled="isStartButtonDisabled" @click="start">
       <PlayIcon class="h-8" />
     </BaseButton>
   </div>
