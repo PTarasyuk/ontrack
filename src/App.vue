4702afbf041cd46bd0f9b20958fabb1ec21466c1
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-11 06:31:04 +0300

    Inject createActivity function into TheActivityForm component

--- src/App.vue
+++ src/App.vue
@@ -64,6 +64,7 @@ function setActivitySecondsToComplete(activity, secondsToComplete) {
 provide('updateTimelineItemActivitySeconds', updateTimelineItemActivitySeconds)
 provide('setActivitySecondsToComplete', setActivitySecondsToComplete)
 provide('setTimelineItemActivity', setTimelineItemActivity)
+provide('createActivity', createActivity)
 provide('periodSelectOptions', generatePeriodSelectOptions())
 provide('activitySelectOptions', activitySelectOptions.value)
 provide('timelineItems', timelineItems.value)
@@ -82,7 +83,6 @@ provide('timelineItems', timelineItems.value)
     <TheActivities
       v-show="currentPage === PAGE_ACTIVITIES"
       :activities="activities"
-      @create-activity="createActivity"
       @delete-activity="deleteActivity"
     />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
