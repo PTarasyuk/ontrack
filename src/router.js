bcac99c1420550f220f3d4c920a263960e43c175
Pavlo Tarasiuk <pasha.tarasyuk@gmail.com>
2025-09-11 07:30:23 +0300

    Router module

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import { ref } from 'vue'
+import { PAGE_TIMELINE } from './constants'
+import { normalizePageHash } from './functions'
+
+export const currentPage = ref(normalizePageHash())
+
+export const timelineRef = ref()
+
+export function navigate(page) {
+  if (currentPage.value == PAGE_TIMELINE && page === PAGE_TIMELINE) {
+    timelineRef.value.scrollToHour()
+  }
+  if (page !== PAGE_TIMELINE) {
+    document.body.scrollIntoView()
+  }
+  currentPage.value = page
+}
